ccf5aa40f0f90484d8117153c6454d7bf4d45d30
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-08-03 16:21:23 +0530

    Folder structure for page components was created. Page routes were added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+import Create from "./pages/create/Create";
+import Recipe from "./pages/recipe/Recipe";
+import Search from "./pages/search/Search";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
