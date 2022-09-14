50f522adb7d266b5e71c3e61365920f46215b580
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-14 07:46:11 +0530

    A button to the recipe details page was added to update the document and the implementation of the button was done.

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -34,6 +34,12 @@ export default function Recipe() {
       });
   }, [id]);
 
+  const handleClick = () => {
+    projectFirestore.collection("recipes").doc(id).update({
+      title: "Something completely different!",
+    });
+  };
+
   return (
     <div className={`recipe ${mode}`}>
       {error && <p className="error">{error}</p>}
@@ -48,6 +54,7 @@ export default function Recipe() {
             ))}
           </ul>
           <p className="method">{recipe.method}</p>
+          <button onClick={handleClick}>Update Me</button>
         </>
       )}
     </div>
